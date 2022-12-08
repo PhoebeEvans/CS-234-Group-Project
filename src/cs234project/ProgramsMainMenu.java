1698dc59678cd55c6160c04283bf60bf5b9f0000
jacqeinz <jacquicv.19@gmail.com>
2022-12-07 19:40:14 -0700

    Final GUI

--- src/cs234project/ProgramsMainMenu.java
+++ src/cs234project/ProgramsMainMenu.java
@@ -30,62 +30,59 @@ public class ProgramsMainMenu extends javax.swing.JFrame {
         MainPanel = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         searchBtn = new javax.swing.JButton();
-        editBtn = new javax.swing.JButton();
-        deleteBtn = new javax.swing.JButton();
         exitBtn = new javax.swing.JButton();
         addBtn = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        MainPanel.setFont(new java.awt.Font("Georgia", 0, 12)); // NOI18N
         MainPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel1.setFont(new java.awt.Font("Georgia", 0, 24)); // NOI18N
         jLabel1.setText("Programs Menu");
-        MainPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 120, -1, -1));
+        MainPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 120, -1, -1));
 
-        searchBtn.setText("Search");
+        searchBtn.setFont(new java.awt.Font("Georgia", 0, 14)); // NOI18N
+        searchBtn.setText("Search/Edit Programs");
         searchBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 searchBtnActionPerformed(evt);
             }
         });
-        MainPanel.add(searchBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 210, -1, -1));
+        MainPanel.add(searchBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 290, 190, 60));
 
-        editBtn.setText("Edit");
-        editBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                editBtnActionPerformed(evt);
-            }
-        });
-        MainPanel.add(editBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 300, -1, -1));
-
-        deleteBtn.setText("Delete");
-        deleteBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                deleteBtnActionPerformed(evt);
-            }
-        });
-        MainPanel.add(deleteBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 460, -1, -1));
-
+        exitBtn.setFont(new java.awt.Font("Georgia", 0, 14)); // NOI18N
         exitBtn.setText("Return to Main Menu");
         exitBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 exitBtnActionPerformed(evt);
             }
         });
-        MainPanel.add(exitBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 540, -1, -1));
+        MainPanel.add(exitBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 580, 190, 70));
 
+        addBtn.setFont(new java.awt.Font("Georgia", 0, 14)); // NOI18N
         addBtn.setText("Add");
         addBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addBtnActionPerformed(evt);
             }
         });
-        MainPanel.add(addBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 380, -1, -1));
+        MainPanel.add(addBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 380, 190, 60));
 
-        getContentPane().add(MainPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1030, 850));
+        jButton1.setFont(new java.awt.Font("Georgia", 0, 14)); // NOI18N
+        jButton1.setText("View Catalog");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        MainPanel.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 480, 190, 70));
 
-        setSize(new java.awt.Dimension(1045, 861));
+        getContentPane().add(MainPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 750, 840));
+
+        setSize(new java.awt.Dimension(769, 844));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -95,18 +92,6 @@ public class ProgramsMainMenu extends javax.swing.JFrame {
        ProgramEditFrame.searchLabel.setVisible(true);
        ProgramEditFrame.searchField.setVisible(true);
     }//GEN-LAST:event_searchBtnActionPerformed
-
-    private void editBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editBtnActionPerformed
-        ProgramEditFrame frame = new ProgramEditFrame();
-        frame.setVisible(true);
-        ProgramEditFrame.updateButton.setVisible(true);
-    }//GEN-LAST:event_editBtnActionPerformed
-
-    private void deleteBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteBtnActionPerformed
-        ProgramEditFrame frame = new ProgramEditFrame();
-        frame.setVisible(true);
-        ProgramEditFrame.btnDelete.setVisible(true);
-    }//GEN-LAST:event_deleteBtnActionPerformed
 
     private void exitBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitBtnActionPerformed
         //exit
@@ -119,47 +104,16 @@ public class ProgramsMainMenu extends javax.swing.JFrame {
         add.setVisible(true);
     }//GEN-LAST:event_addBtnActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ProgramsMainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ProgramsMainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ProgramsMainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ProgramsMainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ProgramsMainMenu().setVisible(true);
-            }
-        });
-    }
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       ProgramEditFrame frame = new ProgramEditFrame();
+       frame.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel MainPanel;
     private javax.swing.JButton addBtn;
-    private javax.swing.JButton deleteBtn;
-    private javax.swing.JButton editBtn;
     private javax.swing.JButton exitBtn;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JButton searchBtn;
     // End of variables declaration//GEN-END:variables
